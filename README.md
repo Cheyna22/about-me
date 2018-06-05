da5c222e13fe199227a3c58fec29dcece85f0771
Cheyna Humphries <humphries.c92@gmail.com>
2018-06-05 14:57:58 -0700

    updated js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # about-me
 Creating a new repository for About Me project for 201d36 class.
+
+
+Reset CSS came from Eric Meyer.
+https://meyerweb.com/eric/tools/css/reset/
---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+  alert('Let\'s play a game!');
+  
+  var likeTomatoes = prompt('Do you think I like tomatos?').toLowerCase();
+  if (likeTomatoes === 'yes' || 'y') {
+    alert('Correct! I love tomatoes with salt!');
+  } else {
+    alert('Actually, I do love tomatoes!');
+  }
+  console.log('The user answered: ' + likeTomatoes);
+
+  var myAge = prompt('Do you think that I am 26 years old?').toLowerCase();
+  if (myAge === 'yes' || 'y') {
+    alert('Yes! I am 26 years old!');
+  } else {
+    alert('That is incorrect! I am 26 years old!');
+  }
+  console.log('The user answered: ' + myAge);
+
+  var favAnimal = prompt('Am I a cat lover?').toLowerCase();
+  if (favAnimal === 'yes' || 'y') {
+    alert('Ooops! I am actuall a dog lover!');
+  } else {
+    alert('You got it, I am NOT a cat lover!');
+  }
+  console.log('The user answered: ' + favAnimal);
+
+  var bigFear = prompt('Is my biggest fear heights?').toLowerCase();
+  if (bigFear === 'yes' || 'y') {
+    alert('No! I love heights!');
+  } else {
+    alert('Correct! My biggest fear is spiders!');
+  }
+  console.log('The user answered: ' + bigFear);
+
+  var visitedAus = prompt('Do you think I have ever visited Australia?').toLowerCase();
+  if (visitedAus === 'yes' || 'y') {
+    alert('I actually have NOT been, but I would love to!');
+  } else {
+    alert('Correct, I have not been to Australia!');
+  }
+  console.log('The user answered: ' + visitedAus);
