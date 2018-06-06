9cda6eaf9d6faf029f2f5479cfe8f56419a93d34
Cheyna Humphries <humphries.c92@gmail.com>
2018-06-05 22:40:06 -0700

    updated all code to turn in project

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # about-me
+
 Creating a new repository for About Me project for 201d36 class.
 
+Answering 5 yes or no questions about me using if/else statements, and printing to console.
+
+Styled the HTML with CSS.
 
 Reset CSS came from Eric Meyer.
 https://meyerweb.com/eric/tools/css/reset/
--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
 'use strict';
 
-alert('Let\'s play a game!');
+alert('Let\'s play a game about me!');
 
 var likeTomatoes = prompt('Do you think I like tomatos?').toLowerCase();
-if (likeTomatoes === 'yes' || 'y') {
+if (likeTomatoes === 'yes') {
   alert('Correct! I love tomatoes with salt!');
 } else {
   alert('Actually, I do love tomatoes!');
 }
-console.log('The user answered: ' + likeTomatoes);
+console.log('Like tomatoes? The user answered: ' + likeTomatoes);
 
 var myAge = prompt('Do you think that I am 26 years old?').toLowerCase();
-if (myAge === 'yes' || 'y') {
-  alert('Yes! I am 26 years old!');
+if (myAge === 'yes') {
+  alert('Correct! I am 26 years old!');
 } else {
   alert('That is incorrect! I am 26 years old!');
 }
-console.log('The user answered: ' + myAge);
+console.log('Am I 26? The user answered: ' + myAge);
 
 var favAnimal = prompt('Am I a cat lover?').toLowerCase();
-if (favAnimal === 'yes' || 'y') {
-  alert('Ooops! I am actuall a dog lover!');
-} else {
+if (favAnimal === 'no') {
   alert('You got it, I am NOT a cat lover!');
+} else {
+  alert('Ooops! I am actually a dog lover!');
 }
-console.log('The user answered: ' + favAnimal);
+console.log('Animal lover? The user answered: ' + favAnimal);
 
 var bigFear = prompt('Is my biggest fear heights?').toLowerCase();
-if (bigFear === 'yes' || 'y') {
-  alert('No! I love heights!');
-} else {
+if (bigFear === 'no') {
   alert('Correct! My biggest fear is spiders!');
-}
-console.log('The user answered: ' + bigFear);
-
-var visitedAus = prompt('Do you think I have ever visited Australia?').toLowerCase();
-if (visitedAus === 'yes' || 'y') {
-  alert('I actually have NOT been, but I would love to!');
 } else {
-  alert('Correct, I have not been to Australia!');
+  alert('Sorry, my biggest fear is spiders!');
 }
-console.log('The user answered: ' + visitedAus);
+console.log('Biggest fear heights? The user answered: ' + bigFear);
+
+var visitedAus = prompt('Have I been to Australia?').toLowerCase();
+if (visitedAus === 'no') {
+  alert('Correct, I have not been to Australia!');
+} else {
+  alert('That\'s wrong! I have not been to Australia.');
+}
+console.log('Been to australia? The user answered: ' + visitedAus);
